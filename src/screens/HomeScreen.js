f10cc3e4fc57609aca8251e57208ad1f3aa761f0
talaaz <tala.1997.az@gmail.com>
2022-03-23 23:43:29 +0100

    Victory visualization first try

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {Button, Text, View} from 'react-native';
 import bankdata from '../data/csvjson.json';
 import {useState} from 'react';
+import {VictoryBar, VictoryChart, VictoryTheme} from 'victory-native';
 
 const HomeScreen = ({navigation}) => {
-  const [text, setText] = useState();
-
   const dataset = bankdata;
-  //const customData = require('./customData.json');
+
   const data_reformat = dataset.map((data, index) => ({
     ...data,
     AmountNeg: Object.entries(dataset[index])[4][1] * -1,
@@ -23,15 +22,26 @@ const HomeScreen = ({navigation}) => {
     //Object.entries(dataset[index])[0][1].slice(0,2)+"-"+Object.entries(dataset[index])[0][1].slice(3,5)+"-"+Object.entries(dataset[index])[0][1].slice(6,10)
   }));
 
+  const data = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 19000},
+  ];
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <VictoryChart width={350} theme={VictoryTheme.material}>
+        <VictoryBar data={data_reformat} x="Amount" y="month" />
+      </VictoryChart>
+
       <Text>Home Screen</Text>
       <Button
         title="Go to Details123"
         onPress={() => navigation.navigate('Details')}
       />
 
-      <Text>Text: {console.log(data_reformat[0])}</Text>
+      <Text>Text: {console.log(dataset)}</Text>
     </View>
   );
 };
