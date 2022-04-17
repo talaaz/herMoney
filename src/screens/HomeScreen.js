ed742f1dd068896e4c8aaadfd24ad0d1063e0d84
talaaz <tala.1997.az@gmail.com>
2022-04-17 17:17:45 +0200

    progress circles try

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,6 +22,9 @@ import {
   VictoryBrushContainer,
   VictoryZoomContainer,
 } from 'victory-native';
+import {ProgressBar, Colors} from 'react-native-paper';
+import * as Progress from 'react-native-progress';
+
 import transformedData from '../functions/transaction';
 import {Picker} from '@react-native-picker/picker';
 import {LineChart} from '../component/LineChart';
@@ -73,11 +76,30 @@ const HomeScreen = ({navigation}) => {
 
       <Text style={styles.text}>Spending</Text>
       <PercentageChart />
+      <ProgressBar
+        progress={Math.round((-2100 / 3000) * 100) * 100}
+        color={Colors.red800}
+      />
+      <Progress.Circle size={30} indeterminate={true} />
+      <Progress.Pie progress={0.4} size={50} />
+
       <ScrollView horizontal={true}>
         {distinct_list.map((item, i) => {
           return (
             <View style={styles.card_template}>
               <Text>{myFunction('January', item)}</Text>
+              <Progress.Pie
+                progress={
+                  Math.round((myFunction('January', item) / 3000) * 100) / 100
+                }
+                size={50}
+              />
+              <ProgressBar
+                progress={
+                  Math.round((myFunction('January', item) / 3000) * 100) / 100
+                }
+                color={colorScale[i]}
+              />
               <View
                 style={[
                   styles.text_container,
